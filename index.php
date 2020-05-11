2f2a9c4ae8ae4866d4b0eebc1aba3b7b9f12ec65
Sak-sh-i <dwivedi.sakshi809@gmail.com>
2020-05-12 03:24:53 +0530

    fifth

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
-<?php include('process.php');?>
+<?php
+$Fname = $_POST["Fname"];
+$Lname = $_POST["Lname"];
+?>
 <html>
 <head>
 <title>Personal INFO</title>
 </head>
 <body>
-<form method="post" action="process.php">
+<form method="post" action="<?php echo $PHP_SELF;?>">
 First Name:<input type="text" size="12" maxlength="12" name="Fname"><br />
 Last Name:<input type="text" size="12" maxlength="36" name="Lname"><br /></form>
-<input type="submit" name="submit" value="Submit" />
-</body>
-<?php include('process.php');?>
+<?
+if (isset($_POST['submit'])) {
 
+    //Data
+    
+    $Fname = $_POST('Fname');
+    $Lname = $_POST('Lname');
+    
+    echo "Hello, ".$Fname." ".$Lname.".<br />";
+
+    header('Location:index.html');
+}
+?>
