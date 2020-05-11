7d19b5a4c5a26f910b31a59aeb44ab709248e490
Sak-sh-i <dwivedi.sakshi809@gmail.com>
2020-05-12 03:28:23 +0530

    sixth

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $Lname = $_POST["Lname"];
 <form method="post" action="<?php echo $PHP_SELF;?>">
 First Name:<input type="text" size="12" maxlength="12" name="Fname"><br />
 Last Name:<input type="text" size="12" maxlength="36" name="Lname"><br /></form>
+<input type="submit" name="submit" id="submit" value="Submit">
 <?
 if (isset($_POST['submit'])) {
 
