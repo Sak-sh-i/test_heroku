c7bea5973961435c885474c05a6764c129af524a
Sak-sh-i <dwivedi.sakshi809@gmail.com>
2020-05-12 03:20:26 +0530

    third

--- index.php
+++ index.php
@@ -9,4 +9,5 @@ First Name:<input type="text" size="12" maxlength="12" name="Fname"><br />
 Last Name:<input type="text" size="12" maxlength="36" name="Lname"><br /></form>
 <input type="submit" name="submit" value="Submit" />
 </body>
+<?php include('process.php');?>
 
